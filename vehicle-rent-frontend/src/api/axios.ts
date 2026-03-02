c1dd4158619e4ba6fab76d524cb957d2dc898e44
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2026-03-02 10:54:27 +0530

    Endpoint Change

--- vehicle-rent-frontend/src/api/axios.ts
+++ vehicle-rent-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8180/api',
+    baseURL: 'http://localhost:8180/',
     headers: { 'Content-Type': 'application/json' },
 });
 
