0021deda6e10e6cb03fd7196a2d4226e92f30779
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2026-03-02 12:01:12 +0530

    Endpoint Change

--- vehicle-rent-frontend/src/api/vehicleApi.ts
+++ vehicle-rent-frontend/src/api/vehicleApi.ts
@@ -2,7 +2,7 @@ import api from './axios';
 import type { Vehicle } from '../types';
 
 export const getVehicles = () =>
-    api.get<Vehicle[]>('api/vehicles');
+    api.get<Vehicle[]>('/api/vehicles');
 
 export const getVehicleById = (id: string) =>
-    api.get<Vehicle>(`api/vehicles/${id}`);
+    api.get<Vehicle>(`/api/vehicles/${id}`);
