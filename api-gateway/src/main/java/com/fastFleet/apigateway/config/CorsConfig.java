359e16e87cd1eb47e29db0424e0a7e430e035b24
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2026-03-07 11:14:02 +0530

    Adding

--- api-gateway/src/main/java/com/fastFleet/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/fastFleet/apigateway/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
                 "http://54.159.157.235:*",
-                "https://vehicle-rent-frontend.vercel.app"
+                "https://vehicle-rent-frontend.vercel.app",
+                "http://localhost:5173"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
